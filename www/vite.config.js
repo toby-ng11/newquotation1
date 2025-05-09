98a865e4b1b37daf0615e606d0f8e9c0dfe652cb
tnguyen-tech <tnguyen@centura.ca>
2025-05-09 07:59:17 -0400

    v12.9.3: vite intergrated

---
+++ www/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  root: 'resources/js', // Your source code directory
+  build: {
+    outDir: '../../public/build/assets', // Where to output compiled files
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        home: path.resolve(__dirname, 'resources/js/home.js'),
+        main: path.resolve(__dirname, 'resources/js/p2q.js'),
+      },
+      output: {
+        entryFileNames: '[name].js', // keep it clean
+      },
+    },
+  },
+});
